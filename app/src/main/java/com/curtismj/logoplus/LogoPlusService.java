26987fae068d46e4b1b185e61f1e316decfe84be
Curtis Jones <curtismj.cj@gmail.com>
2019-02-02 20:58:43 +0200

    Switch to using state machine for service

--- app/src/main/java/com/curtismj/logoplus/LogoPlusService.java
+++ app/src/main/java/com/curtismj/logoplus/LogoPlusService.java
@@ -15,6 +15,8 @@ import android.os.Messenger;
 import android.os.PowerManager;
 import android.os.Process;
 import android.util.Log;
+
+import com.curtismj.logoplus.fsm.StateMachine;
 import com.curtismj.logoplus.persist.LogoDao;
 import com.curtismj.logoplus.persist.LogoDatabase;
 import com.curtismj.logoplus.persist.UIState;
@@ -28,8 +30,8 @@ import eu.chainfire.libsuperuser.Shell;
 public class LogoPlusService extends Service {
     public static final int SERVICE_START = 0;
     public static final int NOTIF_PUSH = 1;
-    public static final int ENTER_IDLE = 2;
-    public static final int EXIT_IDLE = 3;
+    public static final int SCREENON = 2;
+    public static final int SCREENOFF = 3;
     public static final int APPLY_EFFECT_MSG = 4;
     public static final int START_BOUNCE = 5;
     public static  final  String START_BROADCAST = BuildConfig.APPLICATION_ID + ".ServiceAlive";
@@ -42,6 +44,18 @@ public class LogoPlusService extends Service {
     public static final int EFFECT_RAINBOW = 3;
     public static final int EFFECT_PINWHEEL = 4;
 
+    public static final int STATE_SCREENON = 0;
+    public static final int STATE_SCREENOFF = 1;
+    public static final int STATE_NOTIF_UPADTE = 2;
+    public static final int STATE_STATE_UPADTE = 3;
+
+    public static final int EVENT_SCREENON = 0;
+    public static final int EVENT_SCREENOFF = 1;
+    public static final int EVENT_NOTIF_UPDATE =  2;
+    public static final int EVENT_NOTIF_UPDATE2 =  3;
+    public static final int EVENT_STATE_UPDATE =  4;
+    public static final int EVENT_STATE_UPDATE2 =  5;
+
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
     private Messenger mMessenger;
@@ -50,13 +64,73 @@ public class LogoPlusService extends Service {
     private String fadeoutBin;
     private PowerManager pm;
     private int[] latestNotifs = new int[0];
-    private  boolean inIdle = false;
-    private  boolean inEffectOn = false;
     private  BroadcastReceiver offReceiver;
     private PowerManager.WakeLock handlerLock;
     private LogoDatabase db;
     private LogoDao dao;
     private UIState state;
+    private StateMachine fsm;
+
+    private  void buildFSM() {
+        fsm = new StateMachine();
+
+        fsm.Enter(STATE_SCREENON, new StateMachine.Callback() {
+                    @Override
+                    public void run(StateMachine sm) {
+                        handlerLock.acquire(10000);
+                        runEffect();
+                        handlerLock.release();
+                    }
+                })
+                .Exit(STATE_SCREENON, new StateMachine.Callback() {
+                    @Override
+                    public void run(StateMachine sm) {
+                        handlerLock.acquire(10000);
+                        blankLights();
+                        handlerLock.release();
+                    }
+                })
+                .Enter(STATE_SCREENOFF, new StateMachine.Callback() {
+                    @Override
+                    public void run(StateMachine sm) {
+                        handlerLock.acquire(10000);
+                        if (latestNotifs.length > 0) {
+                            runProgram(MicroCodeManager.notifyProgramBuild(latestNotifs));
+                        } else if (!state.powerSave) {
+                            runEffect();
+                        }
+                        handlerLock.release();
+                    }
+                })
+                .Exit(STATE_SCREENOFF, new StateMachine.Callback() {
+                    @Override
+                    public void run(StateMachine sm) {
+                        handlerLock.acquire(10000);
+                        blankLights();
+                        handlerLock.release();
+                    }
+                })
+                .Enter(STATE_NOTIF_UPADTE, new StateMachine.Callback() {
+                    @Override
+                    public void run(StateMachine sm) {
+                        sm.Event(EVENT_NOTIF_UPDATE2);
+                    }
+                })
+                .Enter(STATE_STATE_UPADTE, new StateMachine.Callback() {
+                    @Override
+                    public void run(StateMachine sm) {
+                        fetchState();
+                        sm.Event(EVENT_STATE_UPDATE2);
+                    }
+                })
+                .Transition(STATE_SCREENOFF, EVENT_SCREENON, STATE_SCREENON)
+                .Transition(STATE_SCREENON, EVENT_SCREENOFF, STATE_SCREENOFF)
+                .Transition(STATE_SCREENOFF, EVENT_NOTIF_UPDATE, STATE_NOTIF_UPADTE)
+                .Transition(STATE_NOTIF_UPADTE, EVENT_NOTIF_UPDATE2, STATE_SCREENOFF)
+                .Transition(STATE_SCREENON, EVENT_STATE_UPDATE, STATE_STATE_UPADTE)
+                .Transition(STATE_STATE_UPADTE, EVENT_STATE_UPDATE2, STATE_SCREENON)
+                .StartAt(pm.isInteractive() ? STATE_SCREENON : STATE_SCREENOFF);
+    }
 
     private void fetchState()
     {
@@ -96,50 +170,25 @@ public class LogoPlusService extends Service {
         rootSession.waitForIdle();
     }
 
-    public void  runEffect()
-    {
+    public void  runEffect() {
         Log.d("debug", "effect start");
-        if (!inEffectOn) {
-            inEffectOn = true;
-            switch (state.passiveEffect)
-            {
-                case EFFECT_NONE:
-                    blankLights();
-                    break;
-                case EFFECT_STATIC:
-                    runProgram(MicroCodeManager.staticProgramBuild(state.passiveColor));
-                    break;
-                case EFFECT_PULSE:
-                    runProgram(MicroCodeManager.pulseProgramBuild(state.effectLength,state.passiveColor));
-                    break;
-                case EFFECT_RAINBOW:
-                    runProgram(MicroCodeManager.rainbowProgramBuild(state.effectLength, false));
-                    break;
-                case EFFECT_PINWHEEL:
-                    runProgram(MicroCodeManager.rainbowProgramBuild(state.effectLength, true));
-                    break;
-            }
 
-        }
-    }
-
-    private void enterIdle()
-    {
-        Log.d("debug", "enter idle state requested");
-        if (!inIdle) {
-            inIdle = true;
-            Log.d("debug", "entering idle state");
-            blankLights();
-        }
-        if (pm.isInteractive())
-        {
-            Log.d("debug", "interactive idle, effect must run");
-            runEffect();
-        }
-        else if (state.powerSave)
-        {
-            inEffectOn = false;
-            blankLights();
+        switch (state.passiveEffect) {
+            case EFFECT_NONE:
+                blankLights();
+                break;
+            case EFFECT_STATIC:
+                runProgram(MicroCodeManager.staticProgramBuild(state.passiveColor));
+                break;
+            case EFFECT_PULSE:
+                runProgram(MicroCodeManager.pulseProgramBuild(state.effectLength, state.passiveColor));
+                break;
+            case EFFECT_RAINBOW:
+                runProgram(MicroCodeManager.rainbowProgramBuild(state.effectLength, false));
+                break;
+            case EFFECT_PINWHEEL:
+                runProgram(MicroCodeManager.rainbowProgramBuild(state.effectLength, true));
+                break;
         }
     }
 
@@ -158,6 +207,88 @@ public class LogoPlusService extends Service {
         rootSession.waitForIdle();
     }
 
+    private void dumpFadeout(String path) throws IOException {
+        OutputStream myOutput = new FileOutputStream(path);
+        byte[] buffer = new byte[1024];
+        int length;
+        InputStream myInput = getAssets().open("fadeout");
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+        myInput.close();
+        myOutput.flush();
+        myOutput.close();
+    }
+
+    private void init()
+    {
+        Log.d(BuildConfig.APPLICATION_ID, "Service Starting");
+        db = LogoDatabase.getInstance(getApplicationContext());
+        dao = db.logoDao();
+        fetchState();
+        if (state == null || !state.serviceEnabled)
+        {
+            stopSelf();
+            return;
+        }
+
+        rootSession = new Shell.Builder().
+                setAutoHandler(false).
+                useSU().
+                setWantSTDERR(true).
+                setWatchdogTimeout(5).
+                setMinimalLogging(true).
+                open(new Shell.OnCommandResultListener() {
+
+                    // Callback to report whether the shell was successfully started up
+                    @Override
+                    public void onCommandResult(int commandCode, int exitCode, List<String> output) {
+                        Log.d("debug", "Shell result: " + commandCode + "," + exitCode);
+                        RootAvail = (exitCode == Shell.OnCommandResultListener.SHELL_RUNNING);
+                    }
+                });
+
+        Log.d("debug", "wait for root shell");
+        rootSession.waitForIdle();
+
+        if (RootAvail) {
+            fadeoutBin = getFilesDir() + "/fadeout";
+            try {
+                dumpFadeout(fadeoutBin);
+            } catch (IOException e) {
+                failOut();
+                return;
+            }
+
+            pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            handlerLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, BuildConfig.APPLICATION_ID + ":ServiceWorkerLock");
+
+            handlerLock.acquire(10000);
+            Log.d("debug", "got root");
+            rootSession.addCommand(new String[]{
+                    "chmod +x " + fadeoutBin,
+                    "echo 111111111 > /sys/class/leds/lp5523:channel0/device/master_fader_leds",
+                    "echo 0 > /sys/class/leds/lp5523:channel0/device/master_fader1",
+                    fadeoutBin
+            });
+            rootSession.waitForIdle();
+            handlerLock.release();
+
+            IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+            intentFilter.addAction(Intent.ACTION_USER_PRESENT);
+            intentFilter.addAction(APPLY_EFFECT);
+            intentFilter.addAction(LogoPlusNotificationListener.START_BROADCAST);
+            offReceiver = new LogoBroadcastReceiver(mServiceHandler);
+            registerReceiver(offReceiver, intentFilter);
+
+            buildFSM();
+            notifyStarted();
+        } else {
+            Log.d("debug", "root was denied");
+            failOut();
+        }
+    }
+
     // Handler that receives messages from the thread
     private final class ServiceHandler extends Handler {
         public ServiceHandler(Looper looper) {
@@ -171,91 +302,27 @@ public class LogoPlusService extends Service {
             if (msg.what != SERVICE_START && !RootAvail) return;
             switch (msg.what) {
                 case SERVICE_START:
-                    Log.d(BuildConfig.APPLICATION_ID, "Service Starting");
-                    db = LogoDatabase.getInstance(getApplicationContext());
-                    dao = db.logoDao();
-                    fetchState();
-                    if (state == null || !state.serviceEnabled)
-                    {
-                        stopSelf();
-                        return;
-                    }
-
-                    rootSession = new Shell.Builder().
-                            setAutoHandler(false).
-                            useSU().
-                            setWantSTDERR(true).
-                            setWatchdogTimeout(5).
-                            setMinimalLogging(true).
-                            open(new Shell.OnCommandResultListener() {
-
-                                // Callback to report whether the shell was successfully started up
-                                @Override
-                                public void onCommandResult(int commandCode, int exitCode, List<String> output) {
-                                    Log.d("debug", "Shell result: " + commandCode + "," + exitCode);
-                                    RootAvail = (exitCode == Shell.OnCommandResultListener.SHELL_RUNNING);
-                                }
-                            });
-                    Log.d("debug", "wait for root shell");
-                    rootSession.waitForIdle();
-                    if (RootAvail) {
-                        handlerLock.acquire(10000);
-                        Log.d("debug", "got root");
-                        rootSession.addCommand(new String[]{
-                                "chmod +x " + fadeoutBin,
-                                "echo 111111111 > /sys/class/leds/lp5523:channel0/device/master_fader_leds",
-                                "echo 0 > /sys/class/leds/lp5523:channel0/device/master_fader1",
-                                fadeoutBin
-                        });
-                        rootSession.waitForIdle();
-                        enterIdle();
-                        handlerLock.release();
-                        notifyStarted();
-                    } else {
-                        Log.d("debug", "root was denied");
-                        failOut();
-                    }
+                    init();
                     break;
 
                 case NOTIF_PUSH:
-                case EXIT_IDLE:
-                    handlerLock.acquire(10000);
                     Log.d("debug", "notif update");
                     Bundle bundle = msg.getData();
-                    latestNotifs = (msg.what == NOTIF_PUSH) ? bundle.getIntArray("colors") : latestNotifs;
+                    latestNotifs = bundle.getIntArray("colors");
                     Log.d("debug", "notifs: " + latestNotifs.length);
-                    if (!pm.isInteractive()) {
-                        Log.d("debug", "not interactive. proceed");
-                        if (latestNotifs.length > 0) {
-                            Log.d("debug", "notifs loading");
-                            inIdle = false;
-                            inEffectOn = false;
-                            runProgram(MicroCodeManager.notifyProgramBuild(latestNotifs));
-                        } else {
-                            Log.d("debug", "request idle, no notifs");
-                            enterIdle();
-                        }
-                    }
-                    handlerLock.release();
+                    fsm.Event(EVENT_NOTIF_UPDATE);
                     break;
 
-                case ENTER_IDLE:
-                    handlerLock.acquire(10000);
-                    Log.d("debug", "idle requested by outside source");
-                    enterIdle();
-                    handlerLock.release();
+                case SCREENON:
+                    fsm.Event(EVENT_SCREENON);
+                    break;
+
+                case SCREENOFF:
+                    fsm.Event(EVENT_SCREENOFF);
                     break;
 
                 case APPLY_EFFECT_MSG:
-                    handlerLock.acquire(10000);
-                    Log.d("debug", "updating effect");
-                    if (inEffectOn) {
-                        inEffectOn = false;
-                        Log.d("debug", "re-run effect");
-                        fetchState();
-                        enterIdle();
-                    }
-                    handlerLock.release();
+                    fsm.Event(EVENT_STATE_UPDATE);
                     break;
 
                 case START_BOUNCE:
@@ -263,22 +330,7 @@ public class LogoPlusService extends Service {
                     break;
 
             }
-
         }
-    }
-
-    public void dumpFadeout(String path) throws IOException {
-
-        OutputStream myOutput = new FileOutputStream(path);
-        byte[] buffer = new byte[1024];
-        int length;
-        InputStream myInput = getAssets().open("fadeout");
-        while ((length = myInput.read(buffer)) > 0) {
-            myOutput.write(buffer, 0, length);
-        }
-        myInput.close();
-        myOutput.flush();
-        myOutput.close();
     }
 
     private static class LogoBroadcastReceiver extends   BroadcastReceiver {
@@ -295,12 +347,12 @@ public class LogoPlusService extends Service {
             switch (intent.getAction()) {
                 case Intent.ACTION_SCREEN_OFF:
                     Log.d("debug", "screen off, request service resume");
-                     msg = serviceHandler.obtainMessage(EXIT_IDLE);
+                     msg = serviceHandler.obtainMessage(SCREENOFF);
                     serviceHandler.sendMessage(msg);
                     break;
                 case Intent.ACTION_USER_PRESENT:
                     Log.d("debug", "user present, request service to idle");
-                    msg = serviceHandler.obtainMessage(ENTER_IDLE);
+                    msg = serviceHandler.obtainMessage(SCREENON);
                     serviceHandler.sendMessage(msg);
                     break;
                 case APPLY_EFFECT:
@@ -319,19 +371,6 @@ public class LogoPlusService extends Service {
 
     @Override
     public void onCreate() {
-
-        fadeoutBin = getFilesDir() + "/fadeout";
-        try {
-            dumpFadeout(fadeoutBin);
-        } catch (IOException e) {
-            failOut();
-            return;
-        }
-
-        pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-
-        handlerLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, BuildConfig.APPLICATION_ID + ":ServiceWorkerLock");
-
         HandlerThread thread = new HandlerThread("ServiceStartArguments",
                 Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
@@ -343,13 +382,6 @@ public class LogoPlusService extends Service {
 
         Message msg = mServiceHandler.obtainMessage(SERVICE_START);
         mServiceHandler.sendMessage(msg);
-
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
-        intentFilter.addAction(Intent.ACTION_USER_PRESENT);
-        intentFilter.addAction(APPLY_EFFECT);
-        intentFilter.addAction(LogoPlusNotificationListener.START_BROADCAST);
-        offReceiver = new LogoBroadcastReceiver(mServiceHandler);
-        registerReceiver(offReceiver, intentFilter);
 
     }
 
---
+++ app/src/main/java/com/curtismj/logoplus/fsm/StateMachine.java
@@ -0,0 +1,65 @@
+package com.curtismj.logoplus.fsm;
+
+import android.telecom.Call;
+
+import androidx.collection.ArrayMap;
+
+public class StateMachine {
+    public interface Callback
+    {
+        void run(StateMachine sm);
+    }
+
+    private int state;
+    // <StateFrom, <Event, StateTo>>
+    private ArrayMap<Integer, ArrayMap<Integer, Integer>> transitions;
+    private ArrayMap<Integer, Callback> exitCallbacks;
+    private ArrayMap<Integer, Callback> enterCallbacks;
+
+    public  StateMachine()
+    {
+        transitions = new ArrayMap<>();
+        exitCallbacks = new ArrayMap<>();
+        enterCallbacks = new ArrayMap<>();
+    }
+
+    public StateMachine StartAt(int initialState)
+    {
+        state = initialState;
+        if (enterCallbacks.containsKey(state)) enterCallbacks.get(state).run(this);
+        return this;
+    }
+
+    public StateMachine Event(int event)
+    {
+        if (!transitions.containsKey(state)) return this;
+        ArrayMap<Integer, Integer> trans = transitions.get(state);
+        if (!trans.containsKey(event)) return this;
+        int oldState = state;
+        state = trans.get(event);
+        if (oldState != state)
+        {
+            if (exitCallbacks.containsKey(oldState)) exitCallbacks.get(oldState).run(this);
+            if (enterCallbacks.containsKey(state)) enterCallbacks.get(state).run(this);
+        }
+        return this;
+    }
+
+    public StateMachine Transition(int oldState, int event, int newState)
+    {
+        if (!transitions.containsKey(oldState)) transitions.put(oldState, new ArrayMap<Integer, Integer>());
+        transitions.get(oldState).put(event, newState);
+        return this;
+    }
+
+    public StateMachine Exit(int state, Callback callback)
+    {
+        exitCallbacks.put(state, callback);
+        return this;
+    }
+    public StateMachine Enter(int state, Callback callback)
+    {
+        enterCallbacks.put(state, callback);
+        return this;
+    }
+}
